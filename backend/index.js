1e0fea3470ccf08fbd8f6292549e41b2c6d0dded
Ajay-S-Biradar <sbmunnu@gmail.com>
2024-05-23 01:40:10 +0530

    Made some vercel deployment changes

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-const express  = require('express');
+const express = require('express');
 const dotenv = require('dotenv');
 const cors = require("cors");
 const bookRoutes = require('./Routes/bookRoutes');
@@ -12,12 +12,14 @@ app.use(express.json());
 app.use(cors());
 connectDB();
 
-app.use('/api/assistant',bookRoutes);
+app.use('/api/assistant', bookRoutes);
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send("Working.... ")
 })
 
-app.listen(PORT,(req,res)=>{
-    console.log('running.... ',process.env.PORT);
-})
+app.listen(PORT, () => {
+    console.log('running.... ', process.env.PORT);
+});
+
+module.exports = app;
